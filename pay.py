7b0f17958635d11b1aa43e1024ffae6cbfc2c333
JohnnyGa <39858494+JohnnyGa@users.noreply.github.com>
2021-02-15 20:07:47 -0500

    beggining of py4e

---
+++ pay.py
@@ -0,0 +1,5 @@
+#pay rate calculator
+hrs=input("enter hours worked")
+rate=input("enter hourly pay rate")
+pay=float(hrs)*float(rate)
+print("pay",pay) 
---
+++ pay2.py
@@ -0,0 +1,16 @@
+sh = input("Enter Hours: ")
+sr = input ("Enter Rate: ")
+try:
+    fh = float (sh)
+    fr = float (sr)
+except:
+    print("Error, please enter numeric input")
+    quit()
+print(fh,fr)
+if fh > 40 :
+    reg = fr * fh
+    otp = (fh-40) * (fr*.5)
+    xp= reg + otp
+else:
+    xp = fh * fr
+print ("pay:",xp)
